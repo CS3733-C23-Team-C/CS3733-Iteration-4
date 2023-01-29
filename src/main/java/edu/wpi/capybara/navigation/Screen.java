7808337e3e2ac323dd92b40c7dd099df7b87544c
David Rosenstein <mineawesomeman@gmail.com>
2023-01-29 11:40:51 -0500

    added resources

--- src/main/java/edu/wpi/capybara/navigation/Screen.java
+++ src/main/java/edu/wpi/capybara/navigation/Screen.java
@@ -2,7 +2,7 @@ package edu.wpi.capybara.navigation;
 
 public enum Screen {
   ROOT("views/Root.fxml"),
-  HOME("views/Home.fxml"),
+  HOME("views/NewHome.fxml"),
   SERVICE_REQUEST("views/ServiceRequest.fxml");
 
   private final String filename;
