908fee997fd9f68677fd09c7bd1f0397624baae8
johnkhall329 <johnk.hall329@gmail.com>
2023-01-30 13:58:22 -0500

    button validation

--- src/main/java/edu/wpi/capybara/controllers/TransportationController.java
+++ src/main/java/edu/wpi/capybara/controllers/TransportationController.java
@@ -29,12 +29,19 @@ public class TransportationController {
   // @FXML private ImageView map;
   @FXML private MFXComboBox<String> dropDown;
 
+  private enum submissionState {
+    BLANK,
+    PROCESSING,
+    DONE
+  };
+
+  private submissionState currState;
+
   private ObservableList<String> options = FXCollections.observableArrayList();
   /** Initialize controller by FXML Loader. */
   @FXML
   public void initialize() {
     System.out.println("I am from TransportationController.");
-
     options.add("Lower Level 2");
     options.add("Lower Level 1");
     options.add("Ground Floor");
@@ -42,6 +49,7 @@ public class TransportationController {
     options.add("Second Floor");
     options.add("Third Floor");
     dropDown.setItems(options);
+    currState = submissionState.BLANK;
 
     //    if (imageVBox.getWidth() / imageVBox.getHeight() <= 1.45) {
     //      map.setFitWidth(imageVBox.getWidth() / 1000);
@@ -73,15 +81,40 @@ public class TransportationController {
         new transportationSubmission(outputID, outputCurrRoom, outputDestRoom);
     App.totalSubmissions.newTransportationSubmission(newSubmission);
     System.out.println(App.totalSubmissions.getTransportationData());
-    currRoom.setText("");
-    destRoom.setText("");
-    idField.setText("");
+    clearFields();
   }
 
-  public void clearFields(ActionEvent actionEvent) throws IOException {
+  public void clearFields() {
     currRoom.setText("");
     destRoom.setText("");
     idField.setText("");
+    currState = submissionState.BLANK;
+  }
+
+  public void validateButton() {
+    boolean valid = false;
+    switch (currState) {
+      case BLANK:
+        if ((!currRoom.getText().equals("")
+                || !destRoom.getText().equals("")
+                || !idField.getText().equals(""))
+            && !(!currRoom.getText().equals("")
+                && !destRoom.getText().equals("")
+                && !idField.getText().equals(""))) currState = submissionState.PROCESSING;
+        break;
+      case PROCESSING:
+        if (currRoom.getText().equals("")
+            && destRoom.getText().equals("")
+            && idField.getText().equals("")) currState = submissionState.BLANK;
+        if (!currRoom.getText().equals("")
+            && !destRoom.getText().equals("")
+            && !idField.getText().equals("")) currState = submissionState.DONE;
+        break;
+      case DONE:
+        valid = true;
+        break;
+    }
+    submitButton.setDisable(!valid);
   }
 
   //  public void resizeImg(ActionEvent actionEvent) throws IOException {
