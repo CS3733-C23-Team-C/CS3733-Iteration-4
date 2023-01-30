b7522c735afc89ff5d314cc6d209e4aebc4a6fac
OwenRago <owen@rago.net>
2023-01-30 16:53:57 -0500

    Start adapting past work to current project

--- src/main/java/edu/wpi/capybara/database/DBUpdate.java
+++ src/main/java/edu/wpi/capybara/database/DBUpdate.java
@@ -22,6 +22,22 @@ public class DBUpdate {
     }
   }
 
+  public static void update(
+          String table, String pk1, String pk2, String attribute, String value, String pkType1, String pkType2) {
+
+    try {
+      Statement stmt = connection.createStatement();
+      String insert =
+              "UPDATE " + table + " SET " + attribute + " = " + value + " WHERE " + pkType1 + " = '" + pk2
+                      + "' & '" + pkType2 + " = '" + pk2 + "'";
+      System.out.println(insert);
+      stmt.execute(insert);
+      stmt.close();
+    } catch (SQLException e) {
+      System.out.println(e);
+    }
+  }
+
   public static void delete(String table, String pk, String pkType) {
     try {
       Statement stmt = connection.createStatement();
--- src/main/java/edu/wpi/capybara/objects/Edge.java
+++ src/main/java/edu/wpi/capybara/objects/Edge.java
@@ -1,70 +1,54 @@
 package edu.wpi.capybara.objects;
 
 import edu.wpi.capybara.database.DBUpdate;
-import edu.wpi.capybara.database.DatabaseConnect;
 
 public class Edge {
-  private Node startNode, endNode;
-  private String edgeID;
+  private String startNode, endNode;
 
-  public Edge(String id) {
-    this.edgeID = id;
-  }
-
-  public Edge(String id, Node startNode, Node endNode) {
-    this.edgeID = id;
+  public Edge(String startNode, String endNode) {
     this.startNode = startNode;
     this.endNode = endNode;
   }
 
-  public Node getStartNode() {
+  public String getStartNode() {
     return startNode;
   }
 
-  private void setStartNode(Node startNode) {
+  private void setStartNode(String startNode) {
     this.startNode = startNode;
+    DBUpdate.update("edge", this.startNode, this.endNode, "node1", startNode, "node1", "node2");
   }
 
-  public Node getEndNode() {
+  public String getEndNode() {
     return endNode;
   }
 
-  public Node getOtherNode(Node node) {
+  public String getOtherNode(Node node) {
     if (node.equals(startNode)) return endNode;
     return startNode;
   }
 
-  private void setEndNode(Node endNode) {
+  private void setEndNode(String endNode) {
     this.endNode = endNode;
+    DBUpdate.update("edge", this.startNode, this.endNode, "node2", endNode, "node1", "node2");
   }
 
-  public void setNodes(Node startNode, Node endNode) {
-    this.setStartNode(startNode);
-    this.setEndNode(endNode);
-    startNode.addEdge(this);
-    endNode.addEdge(this);
-  }
+  //  public void setNodes(Node startNode, Node endNode) {
+  //    this.setStartNode(startNode);
+  //    this.setEndNode(endNode);
+  //    startNode.addEdge(this);
+  //    endNode.addEdge(this);
+  //  }
 
-  public String getEdgeID() {
-    return edgeID;
-  }
-
-  public void setEdgeID() {
-    this.edgeID = edgeID;
-  }
-
-  public void deleteEdge() {
-    // remove from hashmap
-    DBUpdate.delete("l1edges", this.edgeID, "edgeid");
-    DatabaseConnect.deleteEdge(this.edgeID);
-  }
+  //  public void deleteEdge() {
+  //    // remove from hashmap
+  //    DBUpdate.delete("edge", this.edgeID, "edgeid");
+  //    DatabaseConnect.deleteEdge(this.edgeID);
+  //  }
 
   @Override
   public String toString() {
-    return "ID : "
-        + this.edgeID
-        + "\n"
-        + "Starting Node : "
+    return "Starting Node : "
         + this.startNode.getNodeID()
         + "\n"
         + "Ending Node : "
@@ -72,7 +56,7 @@ public class Edge {
         + "\n";
   }
 
-  public boolean equals(Edge e) {
-    return this.edgeID.equals(e.getEdgeID());
-  }
+  //  public boolean equals(Edge e) {
+  //    return this.edgeID.equals(e.getEdgeID());
+  //  }
 }
