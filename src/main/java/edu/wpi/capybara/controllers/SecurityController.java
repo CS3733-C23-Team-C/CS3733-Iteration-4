e3002fd834ff3d6baeacdf521bc9f146fcde2807
acrickards <100895069+acrickards@users.noreply.github.com>
2023-01-31 20:45:35 -0500

    securty page

--- src/main/java/edu/wpi/capybara/controllers/SecurityController.java
+++ src/main/java/edu/wpi/capybara/controllers/SecurityController.java
@@ -1,3 +1,26 @@
 package edu.wpi.capybara.controllers;
 
-public class SecurityController {}
+import edu.wpi.capybara.navigation.Navigation;
+import edu.wpi.capybara.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class SecurityController {
+
+  @FXML public MFXButton SubmitButton;
+
+  @FXML public MFXButton ReturnButton;
+
+  @FXML public TextField Location;
+  @FXML public TextArea Description;
+  @FXML public Stage primaryStage;
+
+  public void back(ActionEvent actionEvent) throws IOException {
+    Navigation.navigate(Screen.HOME);
+  }
+}
