227903d3e4d9ab8f7c744b262b8de4dec8a23084
fortraan <fortraan1221@gmail.com>
2023-01-31 19:26:11 -0500

    Map Editor initial implementation, ready for testing

--- src/main/java/edu/wpi/capybara/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/capybara/controllers/MapEditorController.java
@@ -2,17 +2,21 @@ package edu.wpi.capybara.controllers;
 
 import edu.wpi.capybara.controllers.mapeditor.CellPropertyAdapter;
 import edu.wpi.capybara.controllers.mapeditor.NodePropertyAdapter;
+import edu.wpi.capybara.database.DatabaseConnect;
 import java.util.function.Function;
 import javafx.beans.property.Property;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MapEditorController {
 
   @FXML private TableView<NodePropertyAdapter> tableView;
 
-  public MapEditorController() {
+  @FXML
+  public void initialize() {
     final var nodeIDColumn = createTableColumn("Node ID", NodePropertyAdapter::nodeIDProperty);
     final var xCoordColumn = createTableColumn("X", NodePropertyAdapter::xCoordProperty);
     final var yCoordColumn = createTableColumn("Y", NodePropertyAdapter::yCoordProperty);
@@ -23,6 +27,15 @@ public class MapEditorController {
     tableView
         .getColumns()
         .setAll(nodeIDColumn, xCoordColumn, yCoordColumn, floorColumn, buildingColumn);
+
+    final var nodeMap = DatabaseConnect.getNodes();
+    if (nodeMap == null) {
+      log.error("Problem communicating with the database: Node HashMap is null.");
+    } else {
+      final var nodes = DatabaseConnect.getNodes().values();
+      final var adapters = nodes.stream().map(NodePropertyAdapter::new).toList();
+      tableView.getItems().setAll(adapters);
+    }
   }
 
   private static <S, T> TableColumn<S, T> createTableColumn(
--- src/main/java/edu/wpi/capybara/controllers/mapeditor/NodePropertyAdapter.java
+++ src/main/java/edu/wpi/capybara/controllers/mapeditor/NodePropertyAdapter.java
@@ -4,6 +4,7 @@ import edu.wpi.capybara.objects.Node;
 import javafx.beans.property.adapter.*;
 
 public class NodePropertyAdapter {
+  // these builders are designed to be reused, so we keep them in a bunch of static fields.
   private static final JavaBeanStringPropertyBuilder nodeIDBuilder =
       JavaBeanStringPropertyBuilder.create().name("nodeID");
   private static final JavaBeanIntegerPropertyBuilder xCoordBuilder =
@@ -17,9 +18,6 @@ public class NodePropertyAdapter {
   private static final JavaBeanStringPropertyBuilder buildingBuilder =
       JavaBeanStringPropertyBuilder.create().name("building");
 
-  // TODO: 1/31/23 Do we really need to hold on to this reference?
-  private final Node node;
-
   private final JavaBeanStringProperty nodeID;
   private final JavaBeanIntegerProperty xCoord;
   private final JavaBeanIntegerProperty yCoord;
@@ -30,78 +28,43 @@ public class NodePropertyAdapter {
    * Constructs a new NodePropertyAdapter, for use with JavaFX Property APIs.
    *
    * @param node the node that this adapter is associated with
-   * @throws NoSuchMethodException if Node does not follow the JavaBean naming conventions. this is
-   *     an exceptional condition, as there is no way for this code to continue if it occurs. thus,
-   *     it is handed back to the caller to deal with. this is checked by a unit test, so this error
-   *     shouldn't ever occur at runtime.
    */
   // TODO: 1/31/23 implement said unit test
-  public NodePropertyAdapter(Node node) throws NoSuchMethodException {
-    this.node = node;
-    nodeID = nodeIDBuilder.bean(node).build();
-    xCoord = xCoordBuilder.bean(node).build();
-    yCoord = yCoordBuilder.bean(node).build();
-    floor = floorBuilder.bean(node).build();
-    building = buildingBuilder.bean(node).build();
+  // STOPSHIP: 1/31/23 see above to-do
+  public NodePropertyAdapter(Node node) {
+    try {
+      nodeID = nodeIDBuilder.bean(node).build();
+      xCoord = xCoordBuilder.bean(node).build();
+      yCoord = yCoordBuilder.bean(node).build();
+      floor = floorBuilder.bean(node).build();
+      building = buildingBuilder.bean(node).build();
+    } catch (NoSuchMethodException e) {
+      // this happens if Node does not follow the JavaBean naming conventions. this is an
+      // exceptional condition, as there is no way for this code to continue if it occurs. thus, we
+      // throw a runtime error back to the caller instead of trying to work around it. this is
+      // checked by a unit test, so this error shouldn't ever occur at runtime.
+      throw new IllegalStateException(
+          "Failed to find getter/setter methods of Node. This should not be possible.", e);
+    }
   }
 
   // unfortunately, lombok doesn't seem to support the JavaFX Property conventions. thankfully,
   // IntelliJ does. all of these can be auto-generated.
 
-  public String getNodeID() {
-    return nodeID.get();
-  }
-
-  public void setNodeID(String nodeID) {
-    this.nodeID.set(nodeID);
-  }
-
   public JavaBeanStringProperty nodeIDProperty() {
     return nodeID;
-  }
-
-  public int getXCoord() {
-    return xCoord.get();
-  }
-
-  public void setXCoord(int xCoord) {
-    this.xCoord.set(xCoord);
   }
 
   public JavaBeanIntegerProperty xCoordProperty() {
     return xCoord;
   }
 
-  public int getYCoord() {
-    return yCoord.get();
-  }
-
-  public void setYCoord(int yCoord) {
-    this.yCoord.set(yCoord);
-  }
-
   public JavaBeanIntegerProperty yCoordProperty() {
     return yCoord;
   }
 
-  public String getFloor() {
-    return floor.get();
-  }
-
-  public void setFloor(String floor) {
-    this.floor.set(floor);
-  }
-
   public JavaBeanStringProperty floorProperty() {
     return floor;
-  }
-
-  public String getBuilding() {
-    return building.get();
-  }
-
-  public void setBuilding(String building) {
-    this.building.set(building);
   }
 
   public JavaBeanStringProperty buildingProperty() {
