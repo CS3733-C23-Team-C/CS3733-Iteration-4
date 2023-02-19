b453aaa619c836c1a5ffd006d6f3594ec6563573
fortraan <fortraan1221@gmail.com>
2023-02-18 19:30:35 -0500

    WIP implementation of EdgeGFX

--- src/main/java/edu/wpi/capybara/controllers/mapeditor/ui/gfx/EdgeGFX.java
+++ src/main/java/edu/wpi/capybara/controllers/mapeditor/ui/gfx/EdgeGFX.java
@@ -1,7 +1,25 @@
 package edu.wpi.capybara.controllers.mapeditor.ui.gfx;
 
+import edu.wpi.capybara.controllers.mapeditor.adapters.EdgeAdapter;
+import edu.wpi.capybara.controllers.mapeditor.adapters.NodeAdapter;
+import edu.wpi.capybara.objects.hibernate.NodeEntity;
 import javafx.scene.shape.Line;
+import lombok.Getter;
 
 public class EdgeGFX extends GFXBase {
-    private Line line;
+    private static final String STYLE_CLASS = "mapEditorEdge";
+
+    @Getter
+    private final Line line;
+
+    public EdgeGFX(EdgeAdapter edge) {
+        getStyleClass().add(STYLE_CLASS);
+
+        line = new Line();
+
+        //line.startXProperty().bind(edge.startNodeProperty().map(NodeAdapter::xCoordProperty));
+        //line.startYProperty().bind(edge.startNodeProperty().map(NodeAdapter::yCoordProperty));
+        //line.endXProperty().bind(edge.endNodeProperty().map(NodeAdapter::xCoordProperty));
+        //line.endYProperty().bind(edge.endNodeProperty().map(NodeAdapter::yCoordProperty));
+    }
 }
