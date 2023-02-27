ced7770babcc621bdfd31efa848729487169a511
fortraan <fortraan1221@gmail.com>
2023-02-27 00:50:28 -0500

    Enhanced node names with location name and move lookup

--- src/main/java/edu/wpi/cs3733/C23/teamC/controllers/mapeditor/ui/gfx/NodeGFX.java
+++ src/main/java/edu/wpi/cs3733/C23/teamC/controllers/mapeditor/ui/gfx/NodeGFX.java
@@ -1,7 +1,13 @@
 package edu.wpi.cs3733.C23.teamC.controllers.mapeditor.ui.gfx;
 
+import edu.wpi.cs3733.C23.teamC.Main;
 import edu.wpi.cs3733.C23.teamC.objects.Floor;
+import edu.wpi.cs3733.C23.teamC.objects.hibernate.MoveEntity;
 import edu.wpi.cs3733.C23.teamC.objects.hibernate.NodeEntity;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Comparator;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.value.ObservableBooleanValue;
 import javafx.scene.control.Label;
@@ -25,7 +31,51 @@ public class NodeGFX extends GFXBase {
     translateYProperty().bind(node.ycoordProperty());
 
     label.setLabelFor(circle);
-    label.textProperty().bind(node.nodeIDProperty());
+    label
+        .textProperty()
+        .bind(
+            Bindings.createStringBinding(
+                () -> {
+                  System.out.println("Computing node name.");
+                  final var builder = new StringBuilder(node.getNodeID());
+                  builder.append("\n");
+                  final var moves =
+                      Main.getRepo().getMoves().stream()
+                          .filter(move -> move.getNodeID().equals(node.getNodeID()))
+                          .sorted(Comparator.comparing(MoveEntity::getMovedate));
+                  final var iter = moves.iterator();
+                  MoveEntity lastMove = null, nextMove = null;
+                  final var now = Date.from(Instant.now());
+                  while (iter.hasNext()) {
+                    final var move = iter.next();
+                    if (move.getMovedate().before(now)) lastMove = move;
+                    else {
+                      nextMove = move;
+                      break;
+                    }
+                  }
+
+                  if (lastMove != null && nextMove != null) {
+                    builder.append(lastMove.getLongName());
+                    builder.append(" -> ");
+                    builder.append(nextMove.getLongName());
+                    builder.append(" [");
+                    builder.append(nextMove.getMovedate());
+                    builder.append("]");
+                  } else if (lastMove == null && nextMove != null) {
+                    builder.append("? -> ");
+                    builder.append(nextMove.getLongName());
+                    builder.append(" [");
+                    builder.append(nextMove.getMovedate());
+                    builder.append("]");
+                  } else if (lastMove != null) {
+                    builder.append(lastMove.getLongName());
+                  }
+
+                  return builder.toString();
+                },
+                node.nodeIDProperty(),
+                Main.getRepo().getMoves()));
     label.visibleProperty().bind(showLabel);
     label.managedProperty().bind(showLabel);
 
