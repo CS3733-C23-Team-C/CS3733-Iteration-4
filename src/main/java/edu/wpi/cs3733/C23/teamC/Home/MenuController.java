9b01e861a948206b3b8b1377479de900e9cc70fb
aidansyrgak <aidan.syrgak.02@gmail.com>
2023-02-28 21:12:26 -0500

    w

--- src/main/java/edu/wpi/cs3733/C23/teamC/Home/MenuController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamC/Home/MenuController.java
@@ -128,24 +128,23 @@ public class MenuController {
     Navigation.navigate(Screen.ABOUT_PAGE);
   }
 
-
   public void goToGraph(ActionEvent actionEvent) {
     Navigation.navigate(Screen.GRAPH);
   }
-    public void goToMovePage (ActionEvent actionEvent){
-      Navigation.navigate(Screen.MOVES);
-    }
 
-    public void goToKiosk (ActionEvent actionEvent){
-      KioskDialogController.showDialog();
-    }
+  public void goToMovePage(ActionEvent actionEvent) {
+    Navigation.navigate(Screen.MOVES);
+  }
 
-    public static void messageNotiOn () {
-      sNewMessageCircle.setVisible(true);
-    }
+  public void goToKiosk(ActionEvent actionEvent) {
+    KioskDialogController.showDialog();
+  }
 
-    public static void messageNotiOff () {
-      sNewMessageCircle.setVisible(false);
-    }
+  public static void messageNotiOn() {
+    sNewMessageCircle.setVisible(true);
+  }
 
+  public static void messageNotiOff() {
+    sNewMessageCircle.setVisible(false);
+  }
 }
